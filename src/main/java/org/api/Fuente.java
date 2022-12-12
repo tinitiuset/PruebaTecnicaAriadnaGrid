0b384568b26dcc8b1a156dc8df0128c47902ea55
tinitiuset <tinitiuset@gmail.com>
2022-12-12 17:44:10 +0100

    Added Class Fuente

---
+++ src/main/java/org/api/Fuente.java
@@ -0,0 +1,27 @@
+package org.api;
+
+public class Fuente {
+    int id;
+    String nombre;
+
+    public Fuente(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
